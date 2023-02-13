908257869248b8df4dc78c4f69215b782df536d3
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-14 00:23:03 +0100

    arg check in main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,8 +1,19 @@
 #include "../header/ScalarConverter.class.hpp"
 
+int arg_check(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Wrong amount of arguments" << std::endl;
+		return (0);
+	}
+	return (1);
+}
+
 int main(int argc, char **argv)
 {
-	// arg check
+	if (!arg_check(argc, argv))
+		return (1);
 	// parse string
 	return (0);
 }
