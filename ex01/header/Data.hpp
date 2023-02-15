c3e047915755ec1f817117606869bee40f2b8636
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-15 21:40:55 +0100

    data struct

---
+++ ex01/header/Data.hpp
@@ -0,0 +1,11 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+#include <iostream>
+
+typedef struct data_t
+{
+	std::string	planet;
+} Data;
+
+#endif
--- ex01/header/Serializer.class.hpp
+++ ex01/header/Serializer.class.hpp
@@ -7,15 +7,15 @@ class Serializer
 {
 	private:
 
+		Serializer(void);
+		Serializer(Serializer const &src);
+		Serializer&	operator=(Serializer const &assign);
+		~Serializer(void);
 
 	public:
 
 		int	getSerializer(void) const;
 
-		Serializer(void);
-		Serializer(Serializer const &src);
-		Serializer&	operator=(Serializer const &assign);
-		~Serializer(void);
 };
 
 #endif
