5b1d738f3fab818102e0e11f111eb7f5cc53b92b
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-16 18:18:25 +0100

    switch statement for init of object

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,4 +1,7 @@
 #include "../header/Base.class.hpp"
+#include "../header/A.class.hpp"
+#include "../header/B.class.hpp"
+#include "../header/C.class.hpp"
 #include <cstdlib>
 #include <ctime>
 
@@ -6,9 +9,27 @@
 void generate(void)
 {
 	int	whichObject;
+	Base *base;
 
 	srand((int)time(NULL));
 	whichObject = rand() % 3 + 1;
+	switch (whichObject)
+	{
+		case 1:
+			base = new A();
+			std::cout << "Object A." << std::endl;
+
+		case 2:
+			base = new B();
+			std::cout << "Object B." << std::endl;
+
+		case 3:
+			base = new C();
+			std::cout << "Object C." << std::endl;
+
+		default:
+			std::cerr << "Error when instantiating." << std::endl;
+	}
 	std::cout << whichObject << std::endl;
 }
 
